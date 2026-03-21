82344377331aad1b6a489705517fa7514cb05894
Drew Dwyer <drewdwyer17@gmail.com>
2026-03-20 22:45:17 -0400

    Debug checkpoint (almost working)

---
+++ app/bin/src/main.d
@@ -0,0 +1,20 @@
+bin/src/main.o: src/main.c ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ C:/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stdint.h \
+ C:/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ C:/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ C:/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ C:/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ C:/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ C:/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ C:/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f0/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f0/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/f0/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h

f33edae3f9035c0cc126f0ae8bb4d97fa7adc106
Drew Dwyer <drewdwyer17@gmail.com>
2026-03-21 20:13:12 -0400

    Add file to keep signal generation functions including SOL function.

--- app/src/main.c
+++ app/src/main.c
@@ -1,34 +1,20 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <SignalMgr.h>
 
-/* Use the first pin for sign of life */
-#define OUTPUT_GPIO_SQUAREWAVE_PORT GPIOA
-#define OUTPUT_GPIO_SQUAREWAVE_PIN  GPIO9   /* PA1 */
+#define SIGNOFLIFEPORT GPIOA
+#define SIGNOFLIFEPIN GPIO9   // PA9
 
-static void clock_setup(void) {
-    /* Configure system clock to 48 MHz using 8 MHz external crystal.
-       Trying to use the fastest clock source for sign of life. */
+
+int main(void) 
+{
     rcc_clock_setup_in_hse_8mhz_out_48mhz();
-
-    /* Enable clock for the port used by the square wave output pin */
     rcc_periph_clock_enable(RCC_GPIOA);
-}
-
-int main(void) {
-
-    clock_setup();
-
-    /* Configure the square wave pin as push-pull output, no pull-up/down */
-    gpio_mode_setup(OUTPUT_GPIO_SQUAREWAVE_PORT,
-                    GPIO_MODE_OUTPUT,
-                    GPIO_PUPD_NONE,
-                    OUTPUT_GPIO_SQUAREWAVE_PIN);
-
-    while (1) {
-        /* Toggle the pin continuously to produce a square wave */
-        gpio_toggle(OUTPUT_GPIO_SQUAREWAVE_PORT,
-                    OUTPUT_GPIO_SQUAREWAVE_PIN);
+    while (1)
+    {
+        SignOfLifeSignalAtMaximumSpeed(SIGNOFLIFEPORT, SIGNOFLIFEPIN);
     }
-
     return 0;
 }
+
+

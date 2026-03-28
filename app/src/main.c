61441b35fca33a097b7569ad80bd114df077c3ea
Drew Dwyer <drewdwyer17@gmail.com>
2026-03-28 16:34:03 -0400

    Successful! No response yet, but the oscilloscope shows that the clock rising edge is aligned with the latch enable going low!

--- app/src/main.c
+++ app/src/main.c
@@ -12,23 +12,23 @@
 
 //part 1
 
-#define SPI1_LE_PORT GPIOB//treat the LE signal as the chip select 
-#define SPI1_LE_PIN GPIO12 
+#define SPI2_LE_PORT GPIOB//treat the LE signal as the chip select 
+#define SPI2_LE_PIN GPIO12 
 
-#define SPI1_SP_PORT GPIOC
-#define SPI1_SP_PIN GPIO10
+#define SPI2_SP_PORT GPIOC
+#define SPI2_SP_PIN GPIO10
 
-#define SPI1_MISO_PORT GPIOC
-#define SPI1_MISO_PIN GPIO12//(SD01, d6 on PS side)
+#define SPI2_MISO_PORT GPIOC
+#define SPI2_MISO_PIN GPIO12//(SD01, d6 on PS side)
 
-#define SPI1_CLK_PORT GPIOB
-#define SPI1_CLK_PIN GPIO13
+#define SPI2_CLK_PORT GPIOB
+#define SPI2_CLK_PIN GPIO13
 
-#define SPI1_MOSI_PORT GPIOB
-#define SPI1_MOSI_PIN GPIO15
+#define SPI2_MOSI_PORT GPIOB
+#define SPI2_MOSI_PIN GPIO15
 
 
-// #define SPI1_PS_PIN //what pin? The CPOL (clock polarity) bit controls the idle state value of 
+// #define SPI2_PS_PIN //what pin? The CPOL (clock polarity) bit controls the idle state value of 
 //the clock when no data is being transferred. If 
 //CPOL is reset, the SCK pin has a low-level idle state. If set, high 
 //this configuration is used when MCU is master, NSS signal driven manually with GPIO
@@ -36,37 +36,37 @@
 void pe448spisetup(void)
 {
     //enable the SPI
-    rcc_periph_clock_enable(RCC_SPI1);
+    rcc_periph_clock_enable(RCC_SPI2);
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_GPIOB);
     rcc_periph_clock_enable(RCC_GPIOC); 
 
 
     //need to set the PS SP pin HIGH for serial communication 
-    gpio_mode_setup(SPI1_SP_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SPI1_SP_PIN); 
-    gpio_set(SPI1_SP_PORT, SPI1_SP_PIN); //set SP high for serial communication 
+    gpio_mode_setup(SPI2_SP_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SPI2_SP_PIN); 
+    gpio_set(SPI2_SP_PORT, SPI2_SP_PIN); //set SP high for serial communication 
 
     //now set the pin modes as outputs and AF as specified above
-    gpio_mode_setup(SPI1_CLK_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SPI1_CLK_PIN); 
-    gpio_mode_setup(SPI1_MISO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SPI1_MISO_PIN); 
-    gpio_mode_setup(SPI1_MOSI_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SPI1_MOSI_PIN); 
+    gpio_mode_setup(SPI2_CLK_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SPI2_CLK_PIN); 
+    gpio_mode_setup(SPI2_MISO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SPI2_MISO_PIN); 
+    gpio_mode_setup(SPI2_MOSI_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, SPI2_MOSI_PIN); 
 
-    gpio_mode_setup(SPI1_LE_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SPI1_LE_PIN); //set latch enable (CS) as general output 
+    gpio_mode_setup(SPI2_LE_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SPI2_LE_PIN); //set latch enable (CS) as general output 
 
     //set CS high to prevent data transfer during configuration
-    gpio_set(SPI1_LE_PORT, SPI1_LE_PIN);
+    gpio_set(SPI2_LE_PORT, SPI2_LE_PIN);
 
-    gpio_set_af(SPI1_CLK_PORT, GPIO_AF0, SPI1_CLK_PIN); 
-    gpio_set_af(SPI1_MOSI_PORT, GPIO_AF0, SPI1_MOSI_PIN); 
-    gpio_set_af(SPI1_MISO_PORT, GPIO_AF0, SPI1_MISO_PIN); 
+    gpio_set_af(SPI2_CLK_PORT, GPIO_AF0, SPI2_CLK_PIN); 
+    gpio_set_af(SPI2_MOSI_PORT, GPIO_AF0, SPI2_MOSI_PIN); 
+    gpio_set_af(SPI2_MISO_PORT, GPIO_AF0, SPI2_MISO_PIN); 
 
     //SDO1 phase shifter output is D6 of the PS schematic, need to map to MISO. D6 is mapped to PC12 MCU side
-    // // now set and enable SPI1 to use master mode and 
+    // // now set and enable SPI2 to use master mode and 
     //set the idle state of the clock Low for CPOL = 0 ( TRM "The idle state of SCK must correspond to the polarity selected in the SPIx_CR1 register (by 
     // pulling up SCK if CPOL=1 or pulling down SCK if CPOL=0"
-    spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_8, 0, 1, SPI_CR1_LSBFIRST); 
-    spi_set_data_size(SPI1, SPI_CR2_DS_13BIT); //our command size is 13 bits. 
-    spi_fifo_reception_threshold_16bit(SPI1); //make sure to capture all of the recieve bits 
+    spi_init_master(SPI2, SPI_CR1_BAUDRATE_FPCLK_DIV_16, 0, 1, SPI_CR1_LSBFIRST); 
+    spi_set_data_size(SPI2, SPI_CR2_DS_13BIT); //our command size is 13 bits. 
+    spi_fifo_reception_threshold_16bit(SPI2); //make sure to capture all of the recieve bits 
     
 }
 
@@ -79,10 +79,10 @@ int main(void)
 
     pe448spisetup();
 
-    gpio_clear(SPI1_LE_PORT, SPI1_LE_PIN); //SET LOW FOR TX " TRM It is recommended to enable the SPI slave before the master sends the clock. If not, 
+    gpio_clear(SPI2_LE_PORT, SPI2_LE_PIN); //SET LOW FOR TX " TRM It is recommended to enable the SPI slave before the master sends the clock. If not, 
                                         // undesired data transmission might occur."
 
-    spi_enable(SPI1);
+    spi_enable(SPI2);
     
     double testPhaseShift = 205.3;
     bool optBit = 0;
@@ -94,7 +94,7 @@ int main(void)
     {
 
 
-        if (req.rc != Err_Ok) continue; //builds a command and gives it to the req->packedPhaseShiftCmd structure
+        // if (req.rc != Err_Ok) continue; //builds a command and gives it to the req->packedPhaseShiftCmd structure
 
         uint16_t frame; 
 
@@ -105,13 +105,13 @@ int main(void)
 
         //send the frame
         //1001101000011
-        spi_send(SPI1, frame);
+        spi_send(SPI2, frame);
 
         //read back the received word to clear RXNE / capture response
-        req.phaseShifterResponse = spi_read(SPI1); //SDO2 data changes on rising edge of CLK and is valid on falling edge of CLK.
+        req.phaseShifterResponse = spi_read(SPI2); //SDO2 data changes on rising edge of CLK and is valid on falling edge of CLK.
 
         //release chip select
-        gpio_set(SPI1_LE_PORT, SPI1_LE_PIN);
+        gpio_set(SPI2_LE_PORT, SPI2_LE_PIN);
 
         // validate response
         if (req.phaseShifterResponse != frame) {
@@ -119,7 +119,7 @@ int main(void)
             continue;
         }
 
-        ClearPhaseShiftRequest(&req); 
+        // ClearPhaseShiftRequest(&req); 
 
         //need a debug function to later print the value in req.rc
     }
